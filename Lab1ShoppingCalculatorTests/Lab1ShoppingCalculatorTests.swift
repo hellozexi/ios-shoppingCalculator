882f617796549eaa824a2aa9d633e24688578ba9
mikeliu <liuzexi@wustl.edu>
2020-06-16 19:27:14 -0500

    Initial Commit

---
+++ Lab1ShoppingCalculatorTests/Lab1ShoppingCalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Lab1ShoppingCalculatorTests.swift
+//  Lab1ShoppingCalculatorTests
+//
+//  Created by Mike Liu on 6/16/20.
+//  Copyright © 2020 Mike Liu. All rights reserved.
+//
+
+import XCTest
+@testable import Lab1ShoppingCalculator
+
+class Lab1ShoppingCalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
